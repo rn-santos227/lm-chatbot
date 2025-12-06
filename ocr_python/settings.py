7a7c3e048c5b67d89edffca175506fbc550c4224
rn-santos227 <rnsantos227@gmail.com>
2025-12-06 19:42:04 +0800

    updated settings python file

--- ocr_python/settings.py
+++ ocr_python/settings.py
@@ -1,3 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
+class Settings:
+    MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
+    MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
+    MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+    MINIO_REGION = os.getenv("MINIO_REGION", "us-east-1")
+    MINIO_BUCKET = os.getenv("MINIO_BUCKET", "chatbot-files")
+
+settings = Settings()
