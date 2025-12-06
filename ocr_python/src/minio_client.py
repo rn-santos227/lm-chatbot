6037915c264af85e3abb4c8f63470871d17a03cb
rn-santos227 <rnsantos227@gmail.com>
2025-12-06 19:53:16 +0800

    created minio client python file

---
+++ ocr_python/src/minio_client.py
@@ -0,0 +1,17 @@
+from io import BytesIO
+import boto3
+from settings import settings
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=settings.MINIO_ENDPOINT,
+    aws_access_key_id=settings.MINIO_ACCESS_KEY,
+    aws_secret_access_key=settings.MINIO_SECRET_KEY,
+    region_name=settings.MINIO_REGION,
+)
+
+
+def fetch_file(bucket: str, key: str) -> bytes:
+    """Download a file from MinIO as raw bytes."""
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    return obj["Body"].read()
