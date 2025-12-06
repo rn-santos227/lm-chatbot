2b0dd2f144b54dbc30db3f75407027998bd52b58
rn-santos227 <rnsantos227@gmail.com>
2025-12-06 20:38:17 +0800

    created munio upload file in server

---
+++ server/minio/upload.ts
@@ -0,0 +1,9 @@
+import { WebApp } from "meteor/webapp";
+import { Random } from "meteor/random";
+import { Meteor } from "meteor/meteor";
+import {
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { minioClient } from "./client"
+
+
