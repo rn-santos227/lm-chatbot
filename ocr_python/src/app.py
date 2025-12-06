71008bae32b3beab49a4c901b76e820fe0a31bbb
rn-santos227 <rnsantos227@gmail.com>
2025-12-06 19:58:09 +0800

    added ocr request class in app python file

--- ocr_python/src/app.py
+++ ocr_python/src/app.py
@@ -3,4 +3,9 @@ from pydantic import BaseModel
 from minio_client import fetch_file
 from ocr_engine import run_ocr
 
+app = FastAPI()
 
+class OCRRequest(BaseModel):
+    bucket: str
+    key: str
+    mime_type: str
