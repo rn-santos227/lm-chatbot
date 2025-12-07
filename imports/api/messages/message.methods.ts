e9feb5dd9a5a9465b1a1411ef132e222f279877b
rn-santos227 <rnsantos227@gmail.com>
2025-12-07 20:48:04 +0800

    added messages user send method in message methods file

--- imports/api/messages/message.methods.ts
+++ imports/api/messages/message.methods.ts
@@ -2,4 +2,8 @@ import { Meteor } from "meteor/meteor";
 import { Messages } from "./message.collection";
 import type { MessageDoc } from "./message.types";
 
+Meteor.methods({
+  "messages.userSend"(threadId: string, text: string): string {
 
+  },
+});
