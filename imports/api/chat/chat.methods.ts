f79a2f3ad61cf0179833ebceec0d147b6aa4000e
rn-santos227 <rnsantos227@gmail.com>
2025-12-07 20:24:17 +0800

    added chat create method in chat methods file

--- imports/api/chat/chat.methods.ts
+++ imports/api/chat/chat.methods.ts
@@ -2,4 +2,18 @@ import { Meteor } from "meteor/meteor";
 import { Chats } from "./chat.collection";
 import type { ChatThread } from "./chat.type"
 
+Meteor.methods({
+  "chats.create"(title: string): string {
+    const now = new Date();
 
+    const threadId = Chats.insert({
+      title: "New Chat",
+      createdAt: now,
+      updatedAt: now,
+      model: Meteor.settings.lmstudio.model,
+      temperature: 0.2,
+    });
+
+    return threadId;
+  },
+});
