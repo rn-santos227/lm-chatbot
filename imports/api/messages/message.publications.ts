1adae253753d23172c64c77e979a55c1d2bf2172
rn-santos227 <rnsantos227@gmail.com>
2025-12-07 20:53:28 +0800

    added messaage latest publication in message publication file

--- imports/api/messages/message.publications.ts
+++ imports/api/messages/message.publications.ts
@@ -1,2 +1,19 @@
 import { Meteor } from "meteor/meteor";
 import { Messages } from "./message.collection";
+
+Meteor.publish("messages.latest", function (
+  threadId: string,
+  limit: number = 30
+) {
+  if (!threadId || typeof threadId !== "string") {
+    return this.ready();
+  }
+
+  return Messages.find(
+    { threadId },
+    {
+      sort: { createdAt: -1 },
+      limit,
+    }
+  );
+});
